fe086b94ef72f42685444f6dc1717752ba491429
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-01 18:17:49 +0330

    Create Platform.cs

---
+++ eGames/Models/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eGames.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string Description { get; set; }
+    }
+}

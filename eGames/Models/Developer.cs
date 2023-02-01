dbbaf70a71e12e40fa726eb60fe5ff926c1a3af5
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-01 17:38:52 +0330

    Create Developer.cs

---
+++ eGames/Models/Developer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eGames.Models
+{
+    public class Developer
+    {
+        [Key]
+        public int DeveloperId { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Biography { get; set; }
+    }
+}

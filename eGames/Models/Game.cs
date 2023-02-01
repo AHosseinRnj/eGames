ca537b7732bdc140e2b17ad30a6826ac7b06d642
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-01 18:56:03 +0330

    Create Game.cs

---
+++ eGames/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using eGames.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace eGames.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ImageURL { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public GameCategory Category { get; set; }
+    }
+}

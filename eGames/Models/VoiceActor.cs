b0fcc2259a0e32595b826cf2f8f4992974d8a0d2
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-01 17:26:58 +0330

    Create VoiceActor.cs

---
+++ eGames/Models/VoiceActor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eGames.Models
+{
+    public class VoiceActor
+    {
+        [Key] 
+        public int ActorId { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Biography { get; set; }
+    }
+}

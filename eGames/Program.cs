4dc960656957db326ff1318ad310753f78248b6f
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-02 19:37:51 +0330

    Configured SQL server

--- eGames/Program.cs
+++ eGames/Program.cs
@@ -1,9 +1,10 @@
 using eGames.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DbContext configuration
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();

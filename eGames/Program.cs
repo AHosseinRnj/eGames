f1b00fa7e353fa1502e2cb3b0c34ea18acfd292f
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-02 17:00:25 +0330

    Update Program.cs

--- eGames/Program.cs
+++ eGames/Program.cs
@@ -1,4 +1,9 @@
+using eGames.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// DbContext configuration
+builder.Services.AddDbContext<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();

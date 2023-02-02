82bd87fffc5cd4f2bd8f3c7519b7d8b30bf881ee
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-02 16:55:03 +0330

    Create AppDbContext.cs

---
+++ eGames/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eGames.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}

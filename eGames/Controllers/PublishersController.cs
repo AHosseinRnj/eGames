58e69980f691bbd02d4f2337d95d4e238549aad7
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-03 22:11:00 +0330

    Create PublishersController.cs

---
+++ eGames/Controllers/PublishersController.cs
@@ -0,0 +1,22 @@
+﻿using eGames.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eGames.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PublishersController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _appDbContext.Publishers.ToListAsync();
+            return View();
+        }
+    }
+}

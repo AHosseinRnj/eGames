2a046b162db0b58385bedf0f7bd3aa4e447753d4
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-03 22:27:47 +0330

    Create GamesController.cs

---
+++ eGames/Controllers/GamesController.cs
@@ -0,0 +1,22 @@
+﻿using eGames.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eGames.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public GamesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allGames = await _appDbContext.Games.ToListAsync();
+            return View();
+        }
+    }
+}

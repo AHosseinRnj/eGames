90a495aba8d399533bf68d256cd5c10e24f6891d
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-03 21:59:50 +0330

    Create DevelopersController.cs

---
+++ eGames/Controllers/DevelopersController.cs
@@ -0,0 +1,21 @@
+﻿using eGames.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eGames.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DevelopersController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _appDbContext.Developers.ToList();
+            return View();
+        }
+    }
+}

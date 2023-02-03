325be545fa6ae465109d6a156e8c9de9ae3a13f5
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-03 22:21:04 +0330

    Create PlatformsController.cs

---
+++ eGames/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using eGames.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eGames.Controllers
+{
+    public class PlatformsController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PlatformsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;   
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allPlatforms = await _appDbContext.Platforms.ToListAsync();
+            return View();
+        }
+    }
+}

623cb0bf3edca84f85e4a9d3071414b6bfbcfd7e
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-03 23:35:13 +0330

    Update Developer.cs

--- eGames/Models/Developer.cs
+++ eGames/Models/Developer.cs
@@ -7,8 +7,11 @@ namespace eGames.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Profile Picture URL")]
         public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Biography")]
         public string Biography { get; set; }
 
         // Relationships

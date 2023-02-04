ca7108c2ef1544e58dd75a85b05acd5d7e2549fe
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-04 21:38:21 +0330

    Changed default controller to Games

--- eGames/Program.cs
+++ eGames/Program.cs
@@ -28,7 +28,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Games}/{action=Index}/{id?}");
 
 // Seed Database
 AppDbInitializer.Seed(app);

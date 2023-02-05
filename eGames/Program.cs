acf5830c55c31648eea1e195d2fa52e1f9ec8889
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-05 21:30:28 +0330

    Addscoped configuration added

--- eGames/Program.cs
+++ eGames/Program.cs
@@ -1,10 +1,14 @@
 using eGames.Data;
+using eGames.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DbContext configuration
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+// Services configuration
+builder.Services.AddScoped<IDevelopersService, DevelopersService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();

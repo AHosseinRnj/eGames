448ea4e1b12826d1d5a86e2a597b752ab2c423bc
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-05 20:33:04 +0330

    Added IDevelopersService

---
+++ eGames/Data/Services/IDevelopersService.cs
@@ -0,0 +1,13 @@
+﻿using eGames.Models;
+
+namespace eGames.Data.Services
+{
+    public interface IDevelopersService
+    {
+        IEnumerable<Developer> GetDevelopers();
+        Developer GetDeveloperById(int id);
+        void AddDeveloper(Developer developer);
+        void RemoveDeveloper(int id);
+        Developer UpdateDeveloper(int id, Developer newDeveloper);
+    }
+}

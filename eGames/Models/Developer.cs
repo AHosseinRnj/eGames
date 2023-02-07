9b93bdb4acb312b39809b565c6260f82e2fcc497
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-07 18:30:11 +0330

    Added required attribute

--- eGames/Models/Developer.cs
+++ eGames/Models/Developer.cs
@@ -8,10 +8,15 @@ namespace eGames.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
+
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
         public string FullName { get; set; }
+
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Biography { get; set; }
 
         // Relationships

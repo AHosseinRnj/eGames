116e921215165706e64742b9b5d667a9f74ad6c4
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-10 22:52:52 +0330

    Added the "Create" view for a Game

--- eGames/Controllers/GamesController.cs
+++ eGames/Controllers/GamesController.cs
@@ -1,5 +1,6 @@
 ﻿using eGames.Data;
 using eGames.Data.Services;
+using eGames.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,6 +25,22 @@ namespace eGames.Controllers
             var allGames = await _gamesService.GetAllAsync(game => game.Platform);
             return View(allGames);
         }
+
+        // Get: Games/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Create([Bind("ImageURL", "Name", "Description", "Price", "ReleaseDate", "Category")] Game game)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return View(game);
+
+        //    await _gamesService.AddAsync(game);
+        //    return RedirectToAction("Index");
+        //}
 
         // Get: Games/Details/(id)
         public async Task<IActionResult> Details(int id)
---
+++ eGames/Data/ViewModels/NewGameVM.cs
@@ -0,0 +1,47 @@
+﻿using eGames.Data.Base;
+using eGames.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eGames.Models
+{
+    public class NewGameVM
+    {
+        [Display(Name = "Game image URL")]
+        [Required(ErrorMessage = "Image URL is required")]
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Game name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Game description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        [Display(Name = "Price in $")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required(ErrorMessage = "Release Date is required")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Select a category")]
+        [Required(ErrorMessage = "Category is required")]
+        public GameCategory Category { get; set; }
+
+        // Relationships
+        [Display(Name = "Select developer(s)")]
+        [Required(ErrorMessage = "Game developer(s) is required")]
+        public List<int> DeveloperIds { get; set; }
+
+        [Display(Name = "Select a platform")]
+        [Required(ErrorMessage = "Platform is required")]
+        public int PlatformId { get; set; }
+
+        [Display(Name = "Select a publisher")]
+        [Required(ErrorMessage = "publisher is required")]
+        public int PublisherId { get; set; }
+    }
+}

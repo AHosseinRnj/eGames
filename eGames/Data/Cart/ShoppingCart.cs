ebe550cdafcf2f59ecd81d5390879bd57fafb4c5
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-12 20:12:43 +0330

    Added Shopping Cart model

---
+++ eGames/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using eGames.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eGames.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _appDbContext { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<ShoppingCartItem>> GetShoppingCartItemsAsync()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = await _appDbContext.ShoppingCartItems
+                                            .Where(cart => cart.ShoppingCartId == ShoppingCartId)
+                                            .Include(cart => cart.Game).ToListAsync());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            return _appDbContext.ShoppingCartItems.Where(cart => cart.ShoppingCartId == ShoppingCartId)
+                                            .Select(cart => cart.Game.Price * cart.Amount).Sum();
+        }
+    }
+}

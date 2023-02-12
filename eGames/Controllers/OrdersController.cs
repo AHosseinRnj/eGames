7ddbf84e4ba0ddd5f3224f744000d77e81dd5078
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-12 23:29:26 +0330

    Added Orders Controller

---
+++ eGames/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using eGames.Data.Cart;
+using eGames.Data.Services;
+using eGames.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eGames.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IGamesService _gamesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IGamesService gamesService, ShoppingCart shoppingCart)
+        {
+            _gamesService = gamesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _shoppingCart.GetShoppingCartItemsAsync();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ eGames/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using eGames.Data.Cart;
+
+namespace eGames.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}

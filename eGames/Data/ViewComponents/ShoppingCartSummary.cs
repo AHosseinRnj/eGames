9727ff87b744486b165277835ee99c2c822d783a
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-13 17:34:56 +0330

    Added Shopping Cart summary View Component

---
+++ eGames/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,21 @@
+﻿using eGames.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eGames.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _shoppingCart.GetShoppingCartItemsAsync();
+            return View(items.Count);
+        }
+    }
+}

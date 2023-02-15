8f8b094cbc63cf1fa5d51c2a279da59d9060a6f5
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-15 18:44:33 +0330

    Added AccountController

---
+++ eGames/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using eGames.Data;
+using eGames.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eGames.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _appDbContext;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext appDbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

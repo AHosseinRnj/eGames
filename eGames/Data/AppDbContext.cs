73de724a3006257d3dba359a9d0cd03ba959e368
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-02-15 17:26:02 +0330

    Added Identity

--- eGames/Data/AppDbContext.cs
+++ eGames/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using eGames.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace eGames.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
---
+++ eGames/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace eGames.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+    }
+}
